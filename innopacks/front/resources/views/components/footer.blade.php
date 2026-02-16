81a97c0942d5c362c36739efccacab0636422cac
Gbiyede Victor <86299991+vixthor@users.noreply.github.com>
2026-02-16 13:32:46 +0100

    chatbot addon

--- innopacks/front/resources/views/components/footer.blade.php
+++ innopacks/front/resources/views/components/footer.blade.php
@@ -104,6 +104,19 @@
       </div>
     </div>
   </div>
+  <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6992f32d60b3f31c41e4ad69/1jhj0br7t';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 </footer>
 
 @hookinsert('layout.footer.bottom')
